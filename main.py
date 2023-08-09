5501d90e432f4baf6709d57a7c7f9ff947e0e255
Shaun Thomas <134566032+shaunthom@users.noreply.github.com>
2023-08-08 23:15:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#codes
+ave_marks = []
+full_line = []
+ave = 0
+low = 10000
+lowest_marks = []
+with open(filename, 'r') as fl:
+
+    for line in fl:
+        if line.strip():
+            line = line.strip()
+            full_line.append(line)
+
+            '''line = line.split(',')
+            #line11 = line.split(',')
+            ave = 0
+            line_list = line[1:]
+            for number in line[1:]:
+                ave += int(number)
+            ave_marks.append(ave)
+            if ave<low:
+                low = ave
+                lowest_marks = line'''
+    
+                
+    f = lambda line: list(map(int, line.split(',')[1:]))
+    required_value = min(full_line, key=f)         
+    return f, required_value
